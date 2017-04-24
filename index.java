fb86477ffa13e2643c47c5279827b04a6cc1ee94
sudhakar <mindq@gmail.com>
2017-04-24 10:44:53 +0530

    added new index.java file

---
+++ index.java
@@ -0,0 +1,27 @@
+package com.selenium;
+
+public class Abstract {
+
+	public static void main(String[] args) {
+		A x=new B();
+		System.out.println(x instanceof A);
+		System.out.println(x instanceof B);
+		x.m1();//why it is displaying B class method?
+		x.m2();//why it is displaying A class method?
+
+	}
+
+}
+
+abstract class A{
+	abstract void m1();
+	void m2(){
+		System.out.println("m1() in class A");
+	}
+}
+
+class B extends A{
+	void m1(){
+		System.out.println("m1() in Class B");
+	}
+}
